746dedc03de82b18d98c0879638246e5a4b94894
The Iron Yard <taylorashtoncox@gmail.com>
2017-08-28 18:12:19 -0400

    Added form

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -4,6 +4,14 @@ export default class CreatePost extends Component {
   render(){
     return (
       <div className="wrapper">
+        <form>
+          <label for="author">Author's Name</label>
+          <input type="text" name="author"/>
+          <label for="title">Blog Title</label>
+          <input type="text" name="title"/>
+          <label for="post">Blog Post</label>
+          <textarea name="post"/>
+        </form>
       </div>
     );
   }
